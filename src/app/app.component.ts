e02fa82e3e7eac8e64f4adb54fb2dea8d71c1370
Aiden Schrock <aidennoelschrock@gmail.com>
2022-01-25 12:47:48 -0600

    feat: initial credit factor functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { concatMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,116 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  userData: Object = {};
+
+  red: Boolean = true;
+  status: string = '';
+  constructor() {
+    this.randomizeValues()
+  }
+
+
+  generateAge(): string {
+    const currentDate = new Date();
+    function randomDate(start: Date, end: Date) {
+      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+    }
+    const creditHistoryStartDate = randomDate(new Date(1990, 0, 1), currentDate)
+    console.log(creditHistoryStartDate)
+    let years = currentDate.getFullYear() - creditHistoryStartDate.getFullYear();
+    let months = currentDate.getMonth() - creditHistoryStartDate.getMonth();
+
+    if (months < 0) {
+      currentDate.setFullYear(currentDate.getFullYear() - 1);
+      months += 12;
+    }
+
+    if (years > 0) {
+      return years + " yr " + months + " mo"
+    } else {
+      return months + " mo"
+    }
+  }
+
+  randomIntFromInterval(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  randomizeValues(): void {
+    const utilization = this.randomIntFromInterval(0, 100) + '%';
+    const paymentHistory = this.randomIntFromInterval(0, 100) + '%';
+    const derogatoryMarks = this.randomIntFromInterval(1, 10);
+    const totalAccounts = this.randomIntFromInterval(1, 30);
+    const hardInquires = this.randomIntFromInterval(0, 10);
+    const age = this.generateAge()
+
+    this.userData = {
+      "Credit Card Utilization": utilization,
+      "Payment History": paymentHistory,
+      "Derogatory Marks": derogatoryMarks,
+      "Age of Credit History": age,
+      "Total Accounts": totalAccounts,
+      "Hard Inquiries": hardInquires,
+    }
+  }
+
+  classStatus(key: string, value: any) {
+    const status = this.CREDIT_API(key, value)
+    switch (status) {
+      case "good":
+        return 'greenClass';
+        break;
+      case "avg":
+        return 'yellowClass';
+        break;
+      case "bad":
+        return 'redClass';
+        break;
+      default:
+        return '';
+        break;
+    }
+  }
+
+  // Used in template to generate factor cards in the order of the userData obj keys
+  sortInOrder() { return 0 }
+
+  insightNumber(): number {
+    let insights = 0;
+    for (const [key, value] of Object.entries(this.userData)) {
+      if (this.CREDIT_API(key, value) != 'good') {
+        insights += 1;
+      }
+    }
+
+    return insights
+  }
+
+  CREDIT_API = (detail: any, title: any): string | void => {
+    let data;
+    if (detail !== "Age of Credit History") {
+      data = parseInt(title);
+    } else {
+      let years = title.split(" ")[0];
+      return years < 1 ? "bad" : years < 3 ? "avg" : "good";
+    }
+    if (detail === "Credit Card Utilization") {
+      return data < 25 ? "good" : data > 50 ? "bad" : "avg";
+    }
+    if (detail === "Total Accounts") {
+      return data > 8 ? "good" : data <= 3 ? "bad" : "avg";
+    }
+    if (detail === "Derogatory Marks") {
+      return data < 2 ? "good" : data <= 5 ? "avg" : "bad";
+    }
+    if (detail === "Payment History") {
+      return data > 80 ? "good" : data >= 60 ? "avg" : "bad";
+    }
+    if (detail === "Hard Inquiries") {
+      return data < 2 ? "good" : data <= 5 ? "avg" : "bad";
+    }
+  };
+
+
+
 }
